8684efc55d36c678f1ca2d66bd7a742864bf2c55
Gin-cell <assasin1290@gmail.com>
2021-01-19 10:10:39 +0200

    123

---
+++ js3/js3.js
@@ -0,0 +1,58 @@
+/* function fun (a, b) {
+    let msg = (a<b) ? -1 : 
+    (a>b) ? 1 :
+    (a=b) ? 0 :
+    'Что-то пошло не так' ;
+    return msg;
+}
+let c = +prompt('pervoe');
+let d = +prompt('pervoe');
+let check = fun(c, d);
+console.log(check); */
+
+
+/* function fac(n) {
+    return (n != 1) ? n * fac(n - 1) : 1;
+  }
+  x = +prompt('4islo')
+
+  alert( fac(x) );  */
+
+
+/*   function ok (a, b, c) {
+      return parseInt(a.toString() + b.toString() + c.toString());
+  }
+
+  a = +prompt('chel');
+  b = +prompt('chel2');
+  c = +prompt('chel2');
+
+  console.log(ok(a, b, c)); */
+
+
+/*   function sqr (a, b) {
+      if (b==0) {
+          return a*a;
+      } else return a*b;
+  }
+
+  a = prompt('1');
+  b = prompt('Тут вторая сторон. Если нажать просто ОК, она будет с значением 0, то есть вам она не нужна и вы долбите квадрат. Понял это сразу как спросил но не получив ответа');
+
+  console.log(sqr(a, b)); */
+
+/* let n=+prompt("Введите натуральноечисло");
+let sum = n-1;
+let temp = n;
+ 
+for(var i = 2; n>1;){
+    if(!(n%i)){
+        sum-=i;
+        n=n/i;
+    } else i++;
+}
+ 
+if(!sum)
+    alert("Число: " +temp +" является совершенным");
+else
+    alert("Число: " +temp +" не является совершенным"); */
