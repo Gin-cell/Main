ab91c57887316fb2978df458e0ee6f8b556707a4
Gin-cell <assasin1290@gmail.com>
2021-01-29 23:54:10 +0200

    b

---
+++ js7/js.js
@@ -0,0 +1,48 @@
+var playList = [
+    {
+      author: "LED ZEPPELIN",
+      song: "STAIRWAY TO HEAVEN"
+    },
+    {
+      author: "QUEEN",
+      song: "BOHEMIAN RHAPSODY"
+    },
+    {
+      author: "LYNYRD SKYNYRD",
+      song: "FREE BIRD"
+    },
+    {
+      author: "DEEP PURPLE",
+      song: "SMOKE ON THE WATER"
+    },
+    {
+      author: "JIMI HENDRIX",
+      song: "ALL ALONG THE WATCHTOWER"
+    },
+    {
+      author: "AC/DC",
+      song: "BACK IN BLACK"
+    },
+    {
+      author: "QUEEN",
+      song: "WE WILL ROCK YOU"
+    },
+    {
+      author: "METALLICA",
+      song: "ENTER SANDMAN"
+    }
+  ];
+
+  
+  const ul = document.createElement("ul");
+  const mainDiv = document.getElementById("content");
+  mainDiv.append(ul);
+  for (let i in playList) {
+    const li = document.createElement("li");
+    const pBtn = document.createElement("p");
+    pBtn.append(playList[i].author);
+    pBtn.append(" : ");
+    pBtn.append(playList[i].song);
+    li.append(pBtn);
+    ul.append(li);
+  }
